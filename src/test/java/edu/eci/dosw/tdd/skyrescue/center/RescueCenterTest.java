7465f2da9150041110f6876d02c7abafd1b13ddb
nataliaRT12 <nart6006812@gmail.com>
2026-09-13 17:05:31 -0500

    test: RED - does not allow registering a valid drone without implementation

---
+++ src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
@@ -0,0 +1,23 @@
+package edu.eci.dosw.tdd.skyrescue.center;
+
+import edu.eci.dosw.tdd.skyrescue.drone.Drone;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RescueCenterTest {
+
+    @Test
+    void shouldRegisterDroneWhenDataIsValid() {
+        // Arrange
+        RescueCenter center = new RescueCenter();
+        Drone drone = new Drone("D1", "Matrice300", 15);
+
+        // Act
+        boolean result = center.addDrone(drone);
+
+        // Assert
+        assertTrue(result);
+    }
+
+}
